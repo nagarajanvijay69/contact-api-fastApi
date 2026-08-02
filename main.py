9b815dc6ac28b6ba64ed8f48cdc31fa4b843b8cf
nagarajanvijay69 <nagarajanvijay69@gmail.com>
2026-08-02 20:18:43 +0530

    creaing folder structure

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-@app.get("/")
-def rootPath():
-    return {"message": "Hello!, Welcome to FastAPI contact project"}
-
-
-@app.get("/contact")  
-def get_contacts():
-    return {"message": "Contacts List..."}
-
-@app.post("/contact")
-def create_contact():
-    return {"message": "contact created successfully!"}
-
-@app.patch("/contact/{id}")
-def update_contact(id: int):
-    return {"message": f"Updated for id: {id}"}
-
-@app.delete("/contact/{id}")
-def delete_contact(id: int):
-    return {"message": f"deleted contact for id: {id}"}
